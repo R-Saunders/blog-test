efa3ff5e188b7f7e0cdcceed9824611b4fe9dc67
R-Saunders <richsaunders@protonmail.com>
2023-11-29 09:39:43 +0000

    fixed it

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,15 +1,18 @@
 export default async function Comments({ postSlug }: { postSlug: string }) {
 	// `/blog/post-1`
-	const WEBSITE_URL = 'http://localhost:3000'
+	const WEBSITE_URL = "http://localhost:3000";
 	let comments = [];
 
 	try {
-		const commentsResult = await fetch(`${WEBSITE_URL}/ap/comments/${postSlug}`, {next: {revalidate: 5}});
+		const commentsResult = await fetch(
+			`${WEBSITE_URL}/api/comments/${postSlug}`,
+			{ next: { revalidate: 5 } }
+		);
 		const response = await commentsResult.json();
-		console.log(response)
-		comments = response.comments.rows
+		console.log(response);
+		comments = response.comments.rows;
 	} catch (err) {
-		console.log(err)
+		console.log(err);
 	}
 
 	return (
@@ -17,24 +20,32 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
 			<h2>| Comments |</h2>
 			<h3>Leave a comment: </h3>
 
-			<form action={`/api/comments/${postSlug}`} method="POST" className="flex flex-col max-w-lg gap-2 text-slate-900">
+			<form
+				action={`/api/comments/${postSlug}`}
+				method="POST"
+				className="flex flex-col max-w-lg gap-2 text-slate-900"
+			>
 				<label htmlFor="username">Name:</label>
 				<input type="text" name="username" />
 
 				<label htmlFor="comment">Your comment:</label>
 				<textarea name="comment" cols={30} rows={10} />
 
-				<button type="submit" className="bg-zinc-200">send comment</button>
+				<button type="submit" className="bg-zinc-200">
+					Post Comment
+				</button>
 			</form>
-			{comments.map((comment: {id: string; comment: string; username: string;}) => {
-				return (
-					<li key={comment.id}>
-						{comment.username} says... 
-						<br/> 
-						{comment.comment}
-					</li>
-				)
-			})}
+			{comments.map(
+				(comment: { id: string; content: string; username: string }) => {
+					return (
+						<li key={comment.id}>
+							{comment.username} says...
+							<br />
+							{comment.content}
+						</li>
+					);
+				}
+			)}
 		</div>
 	);
 }
